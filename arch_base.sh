e4689ab0645cc14d7dbfe545d228386db14f8f03
Nonanonymous24 <sawantaashay24@gmail.com>
2021-03-15 23:18:11 +0530

    can be run from outside chroot now

--- arch_base.sh
+++ arch_base.sh
@@ -17,15 +17,68 @@ PASSWORD="password"         ### Set password for the user
 FIRMWARE="BIOS/MBR"         ### Takes values "BIOS/MBR" and "UEFI/GPT"
 
 ### IF USING BIOS/MBR, set name of block device here (For eg. /dev/sda, /dev/vda)
-BLOCK_DEVICE="/dev/sda"
+BLOCK_DEVICE="/dev/sda"     ### Do not set this variable equal to a partition, rather the drive itself.
+
+CPU="intel"         ### OPTIONS (intel, amd) ##Installs intel by default if not set to "amd"
 
 GPU="qxl"           ### OPTIONS (qxl, intel, amd, nvidia)
                     ### do not use capital letters
-                    ### If on a VM, use "qxl"
+                    ### If on a VM, use "qxl" 
+                    ### Installs qxl by default if not set to nvidia, intel, or amd
+
+KERNEL="linux"      ### OPTIONS (linux, linux-lts, linux-zen, linux-hardened) 
+                    ### Installs the vanilla kernel if not explicitly specified
+
 
 ### SCRIPT START
 
 /bin/echo -e "\e[1;32mStarting install...\e[0m"
+
+# Installing the base system and kernel
+/bin/echo -e "\e[1;32mInstalling base system and $KERNEL kernel...\e[0m"
+
+if [ $KERNEL == "linux-lts" ]; then
+    pacstrap /mnt base linux-lts linux-lts-headers
+elif [ $KERNEL == "linux-zen" ]; then
+    pacstrap /mnt base linux-zen linux-zen-headers
+elif [ $KERNEL == "linux-hardened" ]; then
+    pacstrap /mnt base linux-hardened linux-hardened-headers
+else
+    pacstrap /mnt base linux linux-headers
+fi
+
+sleep 10
+
+/bin/echo -e "\e[1;32mInstalled base system and $KERNEL kernel.\e[0m"
+
+# Installing firmware for processor
+/bin/echo -e "\e[1;32mInstalling firmware for $CPU processor...\e[0m"
+if [ $CPU == "amd" ]; then
+    pacstrap /mnt amd-ucode
+else
+    pacstrap /mnt intel-ucode
+fi
+
+sleep 5 
+
+/bin/echo -e "\e[1;32mInstalled firmware for $CPU processor.\e[0m"
+
+# Installing more packages
+/bin/echo -e "\e[1;32mInstalling more packages...\e[0m"
+pacstrap /mnt nano reflector
+
+sleep 5
+# Generating fstab file
+/bin/echo -e "\e[1;32mGenerating the fstab file...\e[0m"
+genfstab -U /mnt >> /mnt/etc/fstab
+/bin/echo -e "\e[1;32mGenerated the fstab file as follows:\e[0m"
+cat /mnt/etc/fstab
+
+/bin/echo -e "\e[1;32mIf the file looks incorrect to you, stop the installation right here and fix the problem. You'll be chrooted in 20 seconds.\e[0m"
+sleep 20
+
+/bin/echo -e "\e[1;32mChrooting into the new environment...\e[0m"
+arch-chroot /mnt /bin/bash << EOF
 
 # Set the timezone
 /bin/echo -e "\e[1;32mSetting the timezone to $TIMEZONE...\e[0m"
@@ -57,19 +110,19 @@ echo root:$PASSWORD_ROOT | chpasswd
 
 # Install required packages (the list is extensive so make sure to remove what you don't need)
 /bin/echo -e "\e[1;32mInstalling required packages...\e[0m"
-pacman -S --needed --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-firmware xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset git
+pacman -S --needed --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-firmware xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset git
 
 ## Install graphic drivers
-/bin/echo -e "\e[1;32mInstalling graphic drivers...\e[0m"
+/bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
 
-if [ $GPU == "qxl" ]; then
-    pacman -S --needed --noconfirm xf86-video-qxl
-elif [ $GPU == "amd" ]; then
+if [ $GPU == "amd" ]; then
     pacman -S --needed --noconfirm xf86-video-amdgpu
 elif [ $GPU == "intel" ]; then
     pacman -S --needed --noconfirm xf86-video-intel
-else
+elif [ $GPU == "nvidia" ]; then
     pacman -S --needed --noconfirm nvidia nvidia-utils
+else
+    pacman -S --needed --noconfirm xf86-video-qxl
 fi
 
 ## Install grub bootloader
@@ -82,6 +135,10 @@ fi
 
 ## Make grub configuration
 grub-mkconfig -o /boot/grub/grub.cfg
+
+/bin/echo -e "\e[1;32mThe script should detect Windows installation in case of dual-boot, if mounted correctly.\e[0m"
+/bin/echo -e "\e[1;32mIn case you don't see a Windows image in the above output, stop the script and figure out the problem. The script will continue in 10 seconds.\e[0m"
+sleep 10
 
 # Enable services
 /bin/echo -e "\e[1;32mEnabling required services...\e[0m"
@@ -101,11 +158,19 @@ usermod -aG libvirt $USERNAME
 /bin/echo -e "\e[1;32mAdding user to sudoers file...\e[0m"
 echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$USERNAME
 
+### SCRIPT END
+
+EOF
+
+#Unmount the partitions
+/bin/echo -e "\e[1;32mUnmounting partitions...\e[0m"
+umount -R /mnt
 
 /bin/echo -e "\e[1;32mInstall complete!\e[0m"
 /bin/echo -e "\e[1;32mRoot password is set to $PASSWORD_ROOT.\e[0m"
 /bin/echo -e "\e[1;32mLog in with $USERNAME and password $PASSWORD.\e[0m"
-/bin/echo -e "\e[1;32mExit the system, unmount the partitions and reboot.\e[0m"
 
-### SCRIPT END
-
+/bin/echo -e "\e[1;32mIf you're using VirtualBox, type Ctrl+c to stop the script here and shutdown the system.\e[0m"
+/bin/echo -e "\e[1;32mSystem will reboot in 10 seconds.\e[0m"
+sleep 10
+reboot
