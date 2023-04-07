901ebbfd0f4b888753ee823972595b0b717adea2
Nonanonymous24 <sawantaashay24@gmail.com>
2023-04-07 08:49:41 +0530

    configured error handling for arch_base.sh

--- arch_base.sh
+++ arch_base.sh
@@ -5,50 +5,58 @@
 
 TIMEZONE="Asia/Kolkata"
 HOSTNAME="Arch"
-PASSWORD_ROOT="password"      ### Set root password here
+PASSWORD_ROOT="password" ### Set root password here
 
 # User details
-USERNAME="username"         ### Set username with standard *NIX naming rules
-                            ### The script will allow bad names but
-                            ### it is not advised to use them.
-PASSWORD="password"         ### Set password for the user
+USERNAME="username" ### Set username with standard *NIX naming rules
+### The script will allow bad names but
+### it is not advised to use them.
+PASSWORD="password" ### Set password for the user
 
 # For GRUB bootloader (VERY IMPORTANT)
-FIRMWARE="BIOS/MBR"         ### Takes values "BIOS/MBR" and "UEFI/GPT"
+FIRMWARE="BIOS/MBR" ### Takes values "BIOS/MBR" and "UEFI/GPT"
 
 ### IF USING BIOS/MBR, set name of block device here (For eg. /dev/sda, /dev/vda)
-BLOCK_DEVICE="/dev/sda"     ### Do not set this variable equal to a partition, rather the drive itself.
+BLOCK_DEVICE="/dev/sda" ### Do not set this variable equal to a partition, rather the drive itself.
 
-CPU="intel"         ### OPTIONS (intel, amd, none) ##Installs intel by default if not set to "amd"
+CPU="intel" ### OPTIONS (intel, amd, none) ##Installs intel by default if not set to "amd"
 
-GPU="qxl"           ### OPTIONS (qxl, intel, amd, nvidia, none)
-                    ### do not use capital letters
-                    ### If on a VM, use "qxl" 
-                    ### Installs none by default if not set to nvidia, intel, qxl, or amd
+GPU="qxl" ### OPTIONS (qxl, intel, amd, nvidia, none)
+### do not use capital letters
+### If on a VM, use "qxl"
+### Installs none by default if not set to nvidia, intel, qxl, or amd
 
-KERNEL="linux"      ### OPTIONS (linux, linux-lts, linux-zen, linux-hardened) 
-                    ### Installs the vanilla kernel if not explicitly specified
-
+KERNEL="linux" ### OPTIONS (linux, linux-lts, linux-zen, linux-hardened)
+### Installs the vanilla kernel if not explicitly specified
 
 ### SCRIPT START
+
+# Ctrl + c exits entire script
+trap "exit" INT
+
+# if error is reached, script exits
+set -e
 
 /bin/echo -e "\e[1;32mStarting install...\e[0m"
 
 # Syncing time server
 /bin/echo -e "\e[1;32mGetting time from ntp server...\e[0m"
 timedatectl set-ntp true
+timedatectl status
+
+sleep 2
 
 # Installing the base system and kernel
 /bin/echo -e "\e[1;32mInstalling base system and $KERNEL kernel...\e[0m"
 
 if [ $KERNEL == "linux-lts" ]; then
-    pacstrap /mnt base linux-lts linux-lts-headers
+	pacstrap /mnt base linux-lts linux-lts-headers
 elif [ $KERNEL == "linux-zen" ]; then
-    pacstrap /mnt base linux-zen linux-zen-headers
+	pacstrap /mnt base linux-zen linux-zen-headers
 elif [ $KERNEL == "linux-hardened" ]; then
-    pacstrap /mnt base linux-hardened linux-hardened-headers
+	pacstrap /mnt base linux-hardened linux-hardened-headers
 else
-    pacstrap /mnt base linux linux-headers
+	pacstrap /mnt base linux linux-headers
 fi
 
 sleep 10
@@ -56,16 +64,17 @@ sleep 10
 /bin/echo -e "\e[1;32mInstalled base system and $KERNEL kernel.\e[0m"
 
 # Installing firmware for processor
-/bin/echo -e "\e[1;32mInstalling firmware for $CPU processor...\e[0m"
 if [ $CPU == "amd" ]; then
-    pacstrap /mnt amd-ucode
+	/bin/echo -e "\e[1;32mInstalling firmware for $CPU processor...\e[0m"
+	pacstrap /mnt amd-ucode
 elif [ $CPU == "intel" ]; then
-    pacstrap /mnt intel-ucode
+	/bin/echo -e "\e[1;32mInstalling firmware for $CPU processor...\e[0m"
+	pacstrap /mnt intel-ucode
 else
-    /bin/echo -e "\e[1;32mNot installing cpu firmware.\e[0m"
+	/bin/echo -e "\e[1;32mNot installing cpu firmware.\e[0m"
 fi
 
-sleep 5 
+sleep 5
 
 /bin/echo -e "\e[1;32mInstalled firmware for $CPU processor.\e[0m"
 
@@ -74,9 +83,10 @@ sleep 5
 pacstrap /mnt nano vim reflector
 
 sleep 5
+
 # Generating fstab file
 /bin/echo -e "\e[1;32mGenerating the fstab file...\e[0m"
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >>/mnt/etc/fstab
 /bin/echo -e "\e[1;32mGenerated the fstab file as follows:\e[0m"
 cat /mnt/etc/fstab
 
@@ -84,7 +94,13 @@ cat /mnt/etc/fstab
 sleep 20
 
 /bin/echo -e "\e[1;32mChrooting into the new environment...\e[0m"
-arch-chroot /mnt /bin/bash << EOF
+arch-chroot /mnt /bin/bash <<EOF
+
+# Ctrl + c exits entire script
+trap "exit" INT
+
+# If error is reached, script exits
+set -e
 
 # Set the timezone
 /bin/echo -e "\e[1;32mSetting the timezone to $TIMEZONE...\e[0m"
@@ -97,7 +113,7 @@ hwclock --systohc
 
 # Generate locales and update locale.conf
 /bin/echo -e "\e[1;32mGenerating en_US.UTF-8 locale...\e[0m"
-sed -i '171s/.//' /etc/locale.gen
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
@@ -119,15 +135,18 @@ echo root:$PASSWORD_ROOT | chpasswd
 pacman -S --needed --noconfirm grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-firmware xdg-user-dirs xdg-utils inetutils dnsutils bluez bluez-utils alsa-utils pulseaudio bash-completion openssh rsync bridge-utils dnsmasq ipset git
 
 ## Install graphic drivers
-/bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
 
 if [ $GPU == "amd" ]; then
+    /bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
     pacman -S --needed --noconfirm xf86-video-amdgpu
 elif [ $GPU == "intel" ]; then
+    /bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
     pacman -S --needed --noconfirm xf86-video-intel
 elif [ $GPU == "nvidia" ]; then
+    /bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
     pacman -S --needed --noconfirm nvidia nvidia-utils
 elif [ $GPU == "qxl" ]; then
+    /bin/echo -e "\e[1;32mInstalling graphic drivers for $GPU card...\e[0m"
     pacman -S --needed --noconfirm xf86-video-qxl
 else
     /bin/echo -e "\e[1;32mNot installing graphic drivers\e[0m"
@@ -163,9 +182,9 @@ echo $USERNAME:$PASSWORD | chpasswd
 /bin/echo -e "\e[1;32mAdding user to sudoers file...\e[0m"
 echo "$USERNAME ALL=(ALL) ALL" >> /etc/sudoers.d/$USERNAME
 
-### SCRIPT END
-
 EOF
+
+### SCRIPT END
 
 #Unmount the partitions
 /bin/echo -e "\e[1;32mUnmounting partitions...\e[0m"
